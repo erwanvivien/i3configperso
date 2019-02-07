39ef08af0625dc93da7cf23cb4240ebf909938fc
erwanvivien <44021072+erwanvivien@users.noreply.github.com>
2019-02-07 13:07:27 +0100

    Update color.sh

--- confloose/color.sh
+++ confloose/color.sh
@@ -1,8 +1,6 @@
 DP=$(xrandr | grep " connected" | cut -d' ' -f1);
 wei=$(xrandr | grep " connected" | cut -d' ' -f3 | cut -dx -f1);
 hei=$(xrandr | grep " connected" | cut -d' ' -f3 | cut -dx -f2 | cut -d+ -f1);
-i=0;
-xrandr --output $DP --gamma 0.001:0.001:1;
 
 while true; 
 do 
